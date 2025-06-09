4fdac1eee6f588975e5a8335701d2fe5922d7a69
Misael  M <misaelmdev@gmail.com>
2025-06-08 22:15:20 -0400

    fix: addec changed config for deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+# Install dependencies
 bundle install
+
+# Precompile assets
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+
+# Setup database
 bundle exec rails db:migrate
