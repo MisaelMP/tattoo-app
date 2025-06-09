9b265c46bd13fc035dc7c7562f1c21782cde2473
Misael  M <misaelmdev@gmail.com>
2025-06-08 22:02:03 -0400

    fix: added render deployment config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,8 +26,11 @@ Rails.application.configure do
   config.assets.js_compressor = :terser
   config.assets.css_compressor = :sass
 
-  # Allow fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  # Do not fallback to assets pipeline if a precompiled asset is missed.
+  config.assets.compile = false
+
+  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
+  config.asset_host = ENV['ASSET_HOST'] if ENV['ASSET_HOST'].present?
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
