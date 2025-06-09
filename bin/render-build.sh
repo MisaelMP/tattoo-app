02ca68e05494c6eb8f5b03e94c4e4ed071d60e03
Misael  M <misaelmdev@gmail.com>
2025-06-08 23:05:55 -0400

    Update render-build.sh with proper database setup

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,9 +5,11 @@ set -o errexit
 # Install dependencies
 bundle install
 
+# Setup database
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:seed
+
 # Clean and prepare assets
 bundle exec rails assets:clean
 RAILS_ENV=production bundle exec rails assets:precompile
-
-# Setup database
-bundle exec rails db:migrate
